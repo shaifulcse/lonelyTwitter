5302da21af70d47ae32da2a5b362695c745b5c55
Zharkyn Kassenov <kassenov@cs.ualberta.ca>
2017-02-02 18:19:36 -0700

    javadoc lab

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TestClass.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017 Team X, CMPUT301, University of Alberta - All Rights Reserved.
+ * You may use, distribute or modify this code under terms and conditions of Code of Student Behavior at University of Alberta.
+ * You can find a copy o fthe license in this project. Otherwise, please contact cotnact@abc.ca
+ */
+
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by kassenov on 2/2/17.
+ */
+
+public class TestClass {
+}
