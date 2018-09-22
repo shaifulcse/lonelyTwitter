c0b3451029b3c8f0f8272fa13d0d1f47280c0670
shaiful <msacbd@gmail.com>
2018-09-22 14:46:43 -0600

    created Tweet, Normal and ImportantTweet

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -0,0 +1,31 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public abstract class Tweet {
+    protected String message;
+    protected Date date;
+
+    public Tweet(String message){
+        this.message=message;
+        this.date=new Date();
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+    public Date getDate(){
+        return this.date;
+    }
+    public void setMessage(String message)throws TooLongTweetException{
+        if(message.length()>140){
+            throw new TooLongTweetException();
+        }
+        this.message=message;
+    }
+    public void setDate(Date date){
+        this.date=date;
+    }
+
+    public abstract Boolean isImportant();
+}
